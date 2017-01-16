a749b00ba9230adb72ba6b252319a62dc2f8e16b
Don Bytyqi <donisign@icloud.com>
2017-01-16 20:20:04 +0100

    First commit.

--- Simple Calculator/ViewController.swift
+++ Simple Calculator/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var inputNumber: UITextField!
+    @IBOutlet weak var inputNumber1: UITextField!
+    
+    var sum: (Double, Double) -> Double = { return $0 + $1 }
+    var sub: (Double, Double) -> Double = { return $0 - $1 }
+    var mult: (Double, Double) -> Double = { return $0 * $1 }
+    var div: (Double, Double) -> Double = { return $0 / $1 }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        inputNumber.placeholder = "0"
+        inputNumber1.placeholder = "0"
+        inputNumber.keyboardType = .decimalPad
+        inputNumber1.keyboardType = .decimalPad
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func sum(_ sender: Any) {
+        guard let n1 = Double(inputNumber.text!) else { return }
+        guard let n2 = Double(inputNumber1.text!) else { return }
+        print(sum(n1,n2))
+        inputNumber.text = ""
+        inputNumber1.text = ""
     }
-
-
+    
+    @IBAction func sub(_ sender: Any) {
+        guard let n1 = Double(inputNumber.text!) else { return }
+        guard let n2 = Double(inputNumber1.text!) else { return }
+        print(sub(n1,n2))
+        inputNumber.text = ""
+        inputNumber1.text = ""
+    }
+    @IBAction func mult(_ sender: Any) {
+        guard let n1 = Double(inputNumber.text!) else { return }
+        guard let n2 = Double(inputNumber1.text!) else { return }
+        print(mult(n1,n2))
+        inputNumber.text = ""
+        inputNumber1.text = ""
+    }
+    @IBAction func div(_ sender: Any) {
+        guard let n1 = Double(inputNumber.text!) else { return }
+        guard let n2 = Double(inputNumber1.text!) else { return }
+        print(div(n1,n2))
+        inputNumber.text = ""
+        inputNumber1.text = ""
+    }
 }
 
